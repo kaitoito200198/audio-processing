0c02a09b9b14ab66049e9e2dd30082d76f775007
kaitoito <ito.kaito.dev203015@gmail.com>
2024-02-08 06:50:47 -0800

    first created

---
+++ init.bat
@@ -0,0 +1,7 @@
+echo "# audio-processing" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kaitoito200198/audio-processing.git
+git push -u origin main
